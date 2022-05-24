b19fe1f295dd8271ea25e32468df90b284178d9b
Vitaliy <vitaliykurotchenko@gmail.com>
2022-05-24 21:11:47 +0300

    initial commit

---
+++ Domain/Repositories/Abstract/IServiceItemsRepository.cs
@@ -0,0 +1,16 @@
+﻿using OnlinePanelForProjectsControl.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlinePanelForProjectsControl.Domain.Repositories.Abstract
+{
+	public interface IServiceItemsRepository
+	{
+		IQueryable<ServiceItem> GetServiceItems();
+		ServiceItem GetServiceItemById(Guid id);
+		void SaveServiceItem(ServiceItem entity);
+		void DeleteServiceItem(Guid id);
+	}
+}
